ad2d472088bb666832a24d4de705d7289c38d3d0
yasindinc <39594370+yasindinc@users.noreply.github.com>
2019-07-01 15:09:09 +0300

    LoginIndexRegister

--- MusicStoreSites.UI.MVC/App_Start/RouteConfig.cs
+++ MusicStoreSites.UI.MVC/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace MusicStoreSites.UI.MVC
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
--- MusicStoreSites.UI.MVC/Controllers/AccountController.cs
+++ MusicStoreSites.UI.MVC/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using MusicStoreSites.BLL.Abstract;
+using MusicStoreSites.Model;
+using MusicStoreSites.UI.MVC.Tools;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,9 +17,50 @@ namespace MusicStoreSites.UI.MVC.Controllers
             userService = UserService;
         } 
         [HttpGet]
+        [Route("Register")]
         public ActionResult Register()
         {
             return View();
         }
+        [HttpPost]
+        [Route("Register")]
+        public ActionResult Register(User user)
+        {
+            try
+            {
+                userService.Insert(user);
+                bool sonuc = MailHelper.SendConfirmationMail(user.UserName, user.Password, user.Email, user.ID);
+                if(!sonuc)
+                {
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Veritabanı ekleme hatası!";
+                return View();
+            }
+            return RedirectToAction("Login", "Account");
+        }
+        [HttpGet]
+        [Route("Login")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("Login")]
+        public ActionResult Login(User user)
+        {
+            var gelenKullanici = userService.GetUserByLogin(user.UserName, user.Password);
+            if(gelenKullanici!=null)
+            {
+                Session["kullanici"] = gelenKullanici;
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Error = "Kullanıcı Bulunamadı";
+            return View();
+        }
+
     }
 }
